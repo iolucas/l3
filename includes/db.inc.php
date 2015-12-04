57eb18738801e10b9c5d43d9caee683d0aa0a81e
Lucas V. Oliveira <lucas.o@live.com>
2015-12-04 13:06:12 -0200

    prog

---
+++ includes/db.inc.php
@@ -0,0 +1,14 @@
+<?php
+	try
+	{
+		$pdo = new PDO('mysql:host=localhost;dbname=l3db', 'l3manager', 'l3manager');
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$pdo->exec('SET NAMES "utf8"');
+	}
+	catch (PDOException $e)
+	{
+		$error = 'Unable to connect to the database server.';
+		echo $error;
+		//include 'error.html.php';
+		exit();
+	}
---
+++ main_page.html.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+        <title>L3</title>
+        <style>
+            @import url(https://fonts.googleapis.com/css?family=Ubuntu);
+
+            html, body {
+                margin: 0;
+                font-family: "Ubuntu";
+                height: 100%;
+            }   
+
+            body {
+                background: linear-gradient(180deg, rgba(15,70,0,.8), rgba(240,240,240,.8), rgba(15,70,130,.8));
+                background-size: cover;
+                background-repeat: no-repeat;
+
+            }
+
+            div {
+                color: #fff;
+                text-align: center;
+                font-size: 80px;
+            }
+
+        </style>
+	</head>
+
+	<body>
+        <div>L3</div>
+	</body>
+</html>
